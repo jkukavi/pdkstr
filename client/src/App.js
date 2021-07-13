8f5421c1f0e32c99aae33389c1ad1fe48d3d07e7
Jozo Kukavica <jozo.kukavica1@gmail.com>
2021-07-14 00:46:04 +0200

    fix loading

--- client/src/App.js
+++ client/src/App.js
@@ -134,7 +134,7 @@ function App() {
             </p>
           )}
           <div className="cardContainer">
-            {true && (
+            {arrayLoading && (
               <div className="loading array">
                 <div className="loader" />
               </div>
