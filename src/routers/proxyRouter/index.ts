89ca08d01c020a15dec2f99550392fe2c24c48f0
jkukavi <jozo.kukavica@reev.hr>
2022-08-05 09:52:09 +0200

    - improve speed of download

--- src/routers/proxyRouter/index.ts
+++ src/routers/proxyRouter/index.ts
@@ -52,15 +52,23 @@ router.get<string, { engine: Engine; id: string }, {}>(
       const urlInfo = urls[0];
 
       https
-        .get(urlToHttpOptions(urlInfo.url), (resp) => {
-          res.setHeader(
-            "Content-disposition",
-            `attachment; filename="song.mp3"`
-          );
-          res.setHeader("Content-type", "audio/mp3");
+        .get(
+          {
+            ...urlToHttpOptions(urlInfo.url),
+            headers: {
+              Range: "bytes=0-",
+            },
+          },
+          (resp) => {
+            res.setHeader(
+              "Content-disposition",
+              `attachment; filename="song.mp3"`
+            );
+            res.setHeader("Content-type", "audio/mp3");
 
-          ffmpeg().input(resp).outputFormat("mp3").pipe(res);
-        })
+            ffmpeg().input(resp).outputFormat("mp3").pipe(res);
+          }
+        )
         .on("error", () => {
           res.status(400).json("There was an error while downloading.");
         });
