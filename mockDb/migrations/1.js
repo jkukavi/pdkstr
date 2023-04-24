43dbf7cd89c0642fba0be539c816329c314b9d3b
jkukavi <jozo.kukavica@reev.hr>
2023-04-24 18:59:28 +0200

    - fix the script  - ordering of elements of history and favourites when inserting was wrong, causing the elements to be wrongly inserted

--- mockDb/migrations/1.js
+++ mockDb/migrations/1.js
@@ -10,20 +10,24 @@ for (const user of users) {
   // Migrate history field
   const history = user.history;
   if (history && history.length > 0) {
-    const historyDocuments = history.map((item) => ({
-      user_id: ObjectId(user._id),
-      data: item,
-    }));
+    const historyDocuments = history
+      .map((item) => ({
+        user_id: ObjectId(user._id),
+        data: item,
+      }))
+      .reverse();
     db.history.insertMany(historyDocuments);
   }
 
   // Migrate favourites field
   const favourites = user.favourites;
   if (favourites && favourites.length > 0) {
-    const favouritesDocuments = favourites.map((item) => ({
-      user_id: ObjectId(user._id),
-      data: item,
-    }));
+    const favouritesDocuments = favourites
+      .map((item) => ({
+        user_id: ObjectId(user._id),
+        data: item,
+      }))
+      .reverse();
     db.favourites.insertMany(favouritesDocuments);
   }
 }
