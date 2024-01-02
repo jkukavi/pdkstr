ad51810c6f5b2d1ef2161d5c0dff61c2b1873c97
jkukavi <jole@joule.com>
2024-01-02 03:30:10 +0100

    - also print where the binary was found at when testing

--- src/__test__/globalSetup.ts
+++ src/__test__/globalSetup.ts
@@ -1,4 +1,4 @@
-import { MongoMemoryServer } from "mongodb-memory-server-core";
+import { MongoMemoryServer, MongoBinary } from "mongodb-memory-server-core";
 import dotenv from "dotenv";
 import chalk from "chalk";
 
@@ -6,12 +6,19 @@ dotenv.config();
 
 export = async function globalSetup() {
   const instance = await MongoMemoryServer.create();
+
+  const binaryPath = await MongoBinary.getPath(instance.opts.binary);
+
   const uri = instance.getUri().split("://")[1].slice(0, -1);
   (global as any).__MONGOINSTANCE = instance;
   process.env.LOCAL_MONGODB_URL = uri;
   console.log(
     chalk.green(
-      "\n\nThis test is using an in-memory mongoDb instance from mongodb-memory-server.\n(see file: globalSetup.ts)\n"
+      `
+      
+This test is using an in-memory mongoDb instance from mongodb-memory-server.(see file: globalSetup.ts)
+Binary was located at: ${binaryPath}
+      `
     )
   );
 };
