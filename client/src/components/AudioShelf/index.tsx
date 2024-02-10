7428bf1bf4665df9d319c6f08c32341a752ae4e4
jkukavi <jole@joule.com>
2024-02-10 23:51:57 +0100

    - make icon be an svg instead of .png

--- client/src/components/AudioShelf/index.tsx
+++ client/src/components/AudioShelf/index.tsx
@@ -3,10 +3,15 @@ import ExpandableContainer from "./ExpandableContainer";
 import PlayingQueue from "./PlayingQueue";
 import Player from "./Player";
 
+const AdditionalInfo = () => {
+  return <div>Hello</div>;
+};
+
 const AudioShelf = () => {
   return (
     <ExpandableContainer>
       <Player />
+      <AdditionalInfo />
       <PlayingQueue />
     </ExpandableContainer>
   );
