aae2486879a27ce202288cf4b759105c63ef6d6d
Dario Matijak <darix135@gmail.com>
2024-08-02 17:04:25 +0200

    Notifications

---
+++ client/src/consts/theme.tsx
@@ -0,0 +1,31 @@
+const colors = {
+  gray100: "#393838",
+  gray200: "#525252",
+  gray300: "#6e6e6e",
+  black100: "#343434",
+  black200: "#34343",
+  black300: "#03030363",
+  black400: "#131313",
+};
+
+const theme = {
+  button: {
+    backgroundColor: colors.gray100,
+  },
+  NotificationTransform: {
+    backgroundColor: colors.black100,
+  },
+  DropDownContainer: {
+    backgroundColor: colors.black200,
+  },
+  DropDownShelf: {
+    boxShadow: colors.black300,
+    border: colors.gray200,
+    backgroundColor: colors.black100,
+  },
+  DropDownIconActiveStyle: {
+    border: colors.gray200,
+    filter: colors.black400,
+    backgroundColor: colors.gray300,
+  },
+};
