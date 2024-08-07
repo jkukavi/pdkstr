38e6ae568acc5b0f113df5fd42fd969846867555
martinadujmusic <140391939+martinadujmusic@users.noreply.github.com>
2024-08-07 13:39:41 +0200

    Styles for BottomMenu in BottomMenu.tsx file
    -Brought styles from App.css file to the BottomMenu.tsx file

--- client/src/components/BottomMenu.tsx
+++ client/src/components/BottomMenu.tsx
@@ -6,6 +6,7 @@ import history from "icons/history.svg";
 import library from "icons/library.svg";
 import settings from "icons/settings.svg";
 import { useQueryStringWasUpdated } from "helpers/pushToParams";
+import styled from "styled-components";
 
 const NavLinkWithSearch = (props: React.ComponentProps<typeof NavLink>) => {
   useQueryStringWasUpdated();
@@ -16,31 +17,73 @@ const NavLinkWithSearch = (props: React.ComponentProps<typeof NavLink>) => {
   );
 };
 
+const Menu = styled.div`
+  margin-right: calc(100% - 100vw);
+  position: fixed;
+  z-index: 2;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 2.5rem;
+  background-color: #343434;
+  border-top: 1px solid #6b6b6b;
+  box-shadow: 0px -1px 3px 0px #000000;
+  display: flex;
+
+  & a {
+    flex-grow: 1;
+  }
+
+  & a:nth-child(even) {
+    border-left: 1px solid #6b6b6b;
+    border-right: 1px solid #6b6b6b;
+  }
+
+  & a.active {
+    background-color: #454545;
+  }
+`;
+
+const Icon = styled.div`
+  display: inline-flex;
+  width: 100%;
+  height: 100%;
+
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  & img {
+    width: 25px;
+    height: 25px;
+  }
+`;
+
 const BottomMenu = () => {
   return (
-    <div className="bottomMenu">
+    <Menu>
       <NavLinkWithSearch to="/history" activeClassName="active">
-        <div className="icon">
+        <Icon>
           <img src={history} alt="alt"></img>
-        </div>
+        </Icon>
       </NavLinkWithSearch>
       <NavLinkWithSearch exact to="/" activeClassName="active">
-        <div className="icon">
+        <Icon>
           <img src={magnifier} alt="alt"></img>
-        </div>
+        </Icon>
       </NavLinkWithSearch>
 
       <NavLinkWithSearch to="/favourites" activeClassName="active">
-        <div className="icon">
+        <Icon>
           <img src={library} alt="alt"></img>
-        </div>
+        </Icon>
       </NavLinkWithSearch>
       <NavLinkWithSearch exact to="/recommendation" activeClassName="active">
-        <div className="icon">
+        <Icon>
           <img src={settings} alt="alt"></img>
-        </div>
+        </Icon>
       </NavLinkWithSearch>
-    </div>
+    </Menu>
   );
 };
 
