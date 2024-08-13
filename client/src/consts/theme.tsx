06c570cd2db0eeb92ac332eb937129b60bbbaec2
Dario Matijak <darix135@gmail.com>
2024-08-13 11:04:57 +0200

    [KAN-13] Notifications

--- client/src/consts/theme.tsx
+++ client/src/consts/theme.tsx
@@ -1,10 +1,15 @@
 const colors = {
   gray100: "#393838",
+  gray210: "#343434",
 } as const;
 
 export const theme = {
   button: {
     backgroundColor: colors.gray100,
+  },
+
+  notifications: {
+    backgroundColor: colors.gray210,
   },
 } as const;
 
