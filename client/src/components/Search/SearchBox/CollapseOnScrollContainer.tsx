91cef00166f870e15bfdfb2d2c7031390c9e6d30
Martina Dujmusic <140391939+martinadujmusic@users.noreply.github.com>
2024-08-30 14:59:36 +0200

    [KAN-19] SearchBox , [KAN-18 ] Suggestions  (#10)

--- client/src/components/Search/SearchBox/CollapseOnScrollContainer.tsx
+++ client/src/components/Search/SearchBox/CollapseOnScrollContainer.tsx
@@ -1,13 +1,31 @@
 import React from "react";
 import { useScrollingDownContext } from "contexts/ScrollingDown";
+import styled, { css } from "styled-components";
+
+const collapsedProps = css`
+  transition: transform 1s;
+  transform: translate(0, -2.7rem);
+`;
+
+const SearchBoxContainer = styled.div<{ collapsed: boolean }>`
+  position: relative;
+  background-color: ${({ theme }) => theme.searchbox.container.backgroundColor};
+  border-bottom: 1px solid ${({ theme }) => theme.searchbox.borderBottomColor};
+  box-shadow: 0px 1px 3px 0px
+    ${({ theme }) => theme.searchbox.container.boxShadowColor};
+  transition: transform 0.5s;
+  ${({ collapsed }) => collapsed && collapsedProps}
+`;
 
 const CollapseOnScrollContainer = ({ children }: { children: any }) => {
   const scrollingDown = useScrollingDownContext("cardContainer");
 
   return (
-    <div className={`searchBoxContainer ${scrollingDown ? "collapsed" : ""}`}>
-      {children}
-    </div>
+    <>
+      <SearchBoxContainer collapsed={scrollingDown}>
+        {children}
+      </SearchBoxContainer>
+    </>
   );
 };
 
--- client/src/components/Search/SearchBox/Form/InputWithSuggestions.tsx
+++ client/src/components/Search/SearchBox/Form/InputWithSuggestions.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback } from "react";
+import { styled, css } from "styled-components";
 
 import { addRandomKey, debounce } from "helpers";
 
@@ -14,6 +15,75 @@ type Suggestion = {
   string: string;
   key: string;
 };
+
+const InputHolder = styled.div`
+  width: 40vw;
+  height: 2rem;
+  position: relative;
+
+  @media screen and (max-width: 600px) {
+    flex-grow: 1;
+    position: static;
+  }
+
+  & input {
+    width: 100%;
+    height: 100%;
+    font-size: 1.1rem;
+    color: ${({ theme }) => theme.suggestions.input.color};
+    padding-left: 12px;
+    outline: none;
+    border: none;
+    background-color: ${({ theme }) => theme.suggestions.input.backgroundColor};
+    border-radius: 2px 0 0 2px;
+    border: 1px solid ${({ theme }) => theme.suggestions.input.borderColor};
+    transition: width 0.5s;
+  }
+
+  & input:focus {
+    border: 1px solid salmon;
+  }
+`;
+
+const SuggestionsContainer = styled.div`
+  position: absolute;
+  z-index: 1;
+  width: 100%;
+  max-height: 80vh;
+  overflow-y: auto;
+  background-color: ${({ theme }) =>
+    theme.suggestions.container.backgroundColor};
+  box-shadow: 1px 2px 3px
+    ${({ theme }) => theme.suggestions.container.boxShadowColor};
+  bottom: 0;
+  left: 0;
+  transform: translate(0, 100%);
+  padding: 0.4rem 0;
+
+  @media screen and (max-height: 350px) {
+    max-height: 100vh;
+    z-index: 10;
+  }
+
+  & p {
+    cursor: pointer;
+    will-change: transform;
+    font-size: 1.1rem;
+    padding: 0.5rem 0.5rem;
+    overflow: hidden;
+    color: ${({ theme }) => theme.suggestions.container.textColor};
+
+    z-index: 1;
+    display: flex;
+    align-items: center;
+    transform: translate(0);
+  }
+
+  &p:hover {
+    background-color: ${({ theme }) =>
+      theme.suggestions.container.hoveredTextColor};
+  }
+`;
 
 const InputWithSuggestions = () => {
   const [suggestions, setSuggestions] = useState<{
@@ -97,9 +167,8 @@ const InputWithSuggestions = () => {
   };
 
   return (
-    <div className="inputHolder">
+    <InputHolder>
       <input
-        className="input"
         name="searchString"
         id="searchInput"
         placeholder={"Search"}
@@ -115,20 +184,19 @@ const InputWithSuggestions = () => {
         }}
       />
       {suggestions.show && !!suggestions.array.length && (
-        <div className="suggestionsContainer">
+        <SuggestionsContainer>
           {suggestions.array.map((suggestion) => (
             <p
               key={suggestion.key}
-              className="suggestion"
               onMouseDown={onMouseDownSuggestion}
               onMouseUp={submitSuggestion}
             >
               {suggestion.string}
             </p>
           ))}
-        </div>
+        </SuggestionsContainer>
       )}
-    </div>
+    </InputHolder>
   );
 };
 
